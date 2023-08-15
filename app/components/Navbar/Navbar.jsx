90fca163fb3ab7eb55369f5d8f806be8c1361949
eagrcode <elliot.robinson92@hotmail.com>
2023-08-15 12:41:09 +0100

    add logout to Navbar

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -1,23 +1,38 @@
+// styles
 import styles from "./Navbar.module.scss";
 
-import Link from "next/link";
-
+// icons
 import { FaHome } from "react-icons/fa";
 import { IoTimerOutline } from "react-icons/io5";
 import { GiCrossedSwords } from "react-icons/gi";
 
+// supabase client
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+// next
+import Link from "next/link";
 import { cookies } from "next/headers";
 
+// components
+import LogoutButton from "../LogoutButton";
+
 const Navbar = async () => {
+  // init supabase client
   const supabase = createServerComponentClient({ cookies });
+
+  // get session data
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
+  // destructure user data
+  const user = session && session.user;
+
+  // log user details if true
   console.log(session?.user);
 
-  const links = [
+  // init links view for unauthenticated users
+  const unAuthLinks = [
     {
       id: 1,
       title: "Home",
@@ -38,8 +53,36 @@ const Navbar = async () => {
     },
     {
       id: 4,
-      title: session ? "Logout" : "Login",
+      title: "Login",
       url: "/login",
+    },
+  ];
+
+  // init links view for authenticated users
+  const authLinks = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+      icon: <FaHome size={30} />,
+    },
+    {
+      id: 2,
+      title: "Timer",
+      url: "/timer",
+      icon: <IoTimerOutline size={30} />,
+    },
+    {
+      id: 3,
+      title: "Workouts",
+      url: "/workouts",
+      icon: <GiCrossedSwords size={30} />,
+    },
+    {
+      id: 4,
+      title: "Account",
+      url: "/account",
+      icon: <GiCrossedSwords size={30} />,
     },
   ];
 
@@ -47,17 +90,13 @@ const Navbar = async () => {
     <header className={styles.header}>
       <nav className={styles.nav}>
         <ul>
-          {links.map((link) => (
-            <li key={link.id}>
-              <Link href={link.url}>
-                {link.icon}
-                {link.title}
-              </Link>
-            </li>
-          ))}
+          {!session
+            ? unAuthLinks.map((link) => <Link href={link.url}>{link.title}</Link>)
+            : authLinks.map((link) => <Link href={link.url}>{link.title}</Link>)}
+          {session && <LogoutButton />}
         </ul>
-        {session && <p>Hello, user!</p>}
       </nav>
+      {session && <p>Hello, {user.email}</p>}
     </header>
   );
 };
