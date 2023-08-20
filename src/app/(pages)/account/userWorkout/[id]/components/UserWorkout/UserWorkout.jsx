c6d3827d83ceaeedf37d5ac26dc9959413f4d437
eagrcode <elliot.robinson92@hotmail.com>
2023-08-20 17:05:25 +0100

    import context data + WorkoutTimer component

--- src/app/(pages)/account/userWorkout/[id]/components/UserWorkout/UserWorkout.jsx
+++ src/app/(pages)/account/userWorkout/[id]/components/UserWorkout/UserWorkout.jsx
@@ -14,8 +14,12 @@ import { TiDelete } from "react-icons/ti";
 import { AiTwotoneEdit } from "react-icons/ai";
 import { useState } from "react";
 
+// context
+import { useWorkoutMode } from "@/src/context/useWorkoutMode";
+
 // components
 import WorkoutForm from "../../../../components/WorkoutForm/WorkoutForm";
+import WorkoutTimer from "../../../../../workouts/components/WorkoutTimer/WorkoutTimer";
 
 export default function UserWorkout({
   id,
@@ -31,8 +35,17 @@ export default function UserWorkout({
   console.log(workoutData);
   // init local state
   const [isEditMode, setIsEditMode] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+
+  // destructure context
+  const {
+    isWorkoutMode,
+    setIsWorkoutMode,
+    setWorkoutRounds,
+    setWorkoutRoundTime,
+    setWorkoutRestTime,
+    setWorkoutWarmupTime,
+    setSelectedWorkout,
+  } = useWorkoutMode();
 
   // init router
   const router = useRouter();
@@ -57,50 +70,68 @@ export default function UserWorkout({
     setIsEditMode((prev) => !prev);
   };
 
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.topContainer}>
-        <div className={styles.titleContainer}>
-          <h1>{title}</h1>
-          <button onClick={() => handleEditMode()} aria-label="edit">
-            <AiTwotoneEdit size={30} />
-          </button>
-          {/* <p>{isPublic ? "public" : "private"}</p> */}
-        </div>
-        <div className={styles.iconContainer}>
-          <button onClick={() => handleDeleteWorkout(id)} aria-label="delete">
-            <TiDelete size={30} />
-          </button>
-        </div>
-      </div>
+  // handle workout timer start
+  const handleStart = () => {
+    setSelectedWorkout(workoutData);
+    setWorkoutRounds(numberOfRounds);
+    setWorkoutRoundTime(roundTime);
+    setWorkoutRestTime(restTime);
+    setWorkoutWarmupTime(warmupTime);
+    setIsWorkoutMode(true);
+  };
 
-      {!isEditMode ? (
-        <>
-          <div className={styles.container}>
-            <h2>Info</h2>
-            <div>
-              <p>Total {numberOfRounds * roundTime} min</p>
-              <p>{numberOfRounds} rounds</p>
-              <p>{warmupTime} sec / warmup</p>
-              <p>{roundTime} min / round</p>
-              <p>{restTime} sec / rest</p>
-            </div>
+  if (!isWorkoutMode) {
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.topContainer}>
+          <div className={styles.titleContainer}>
+            <h1>{title}</h1>
+            <button onClick={() => handleEditMode()} aria-label="edit">
+              <AiTwotoneEdit size={30} />
+            </button>
+            {/* <p>{isPublic ? "public" : "private"}</p> */}
           </div>
-          <div className={styles.container}>
-            <h2>Combos</h2>
-            {roundInfo.map((round, index) => (
-              <div key={index}>
-                <p>
-                  Round {round.round} - {round.sequence.map((seq) => `${seq} `)}
-                </p>
+          <div className={styles.iconContainer}>
+            <button onClick={() => handleDeleteWorkout(id)} aria-label="delete">
+              <TiDelete size={30} />
+            </button>
+          </div>
+        </div>
+
+        {!isEditMode ? (
+          <>
+            <div className={styles.container}>
+              <h2>Info</h2>
+              <div>
+                <p>Total {numberOfRounds * roundTime} min</p>
+                <p>{numberOfRounds} rounds</p>
+                <p>{warmupTime} sec / warmup</p>
+                <p>{roundTime} min / round</p>
+                <p>{restTime} sec / rest</p>
               </div>
-            ))}
-          </div>
-          <button className={styles.startBtn}>Start</button>
-        </>
-      ) : (
-        <WorkoutForm mode={"edit"} workoutID={id} />
-      )}
-    </div>
-  );
+            </div>
+            <div className={styles.container}>
+              <h2>Combos</h2>
+              {roundInfo.map((round, index) => (
+                <div key={index}>
+                  <p>
+                    Round {round.round} - {round.sequence.map((seq) => `${seq} `)}
+                  </p>
+                </div>
+              ))}
+            </div>
+            <button onClick={handleStart} className={styles.startBtn}>
+              Start
+            </button>
+          </>
+        ) : (
+          <WorkoutForm mode={"edit"} workoutID={id} />
+        )}
+      </div>
+    );
+  }
+
+  if (isWorkoutMode) {
+    return <WorkoutTimer />;
+  }
 }
