4de0918ade9007524ae5288a6942116d6e38e496
eagrcode <elliot.robinson92@hotmail.com>
2023-08-20 15:29:33 +0100

    import WorkoutTimer component + context

--- src/app/(pages)/workouts/workout/[id]/components/Workout.jsx
+++ src/app/(pages)/workouts/workout/[id]/components/Workout.jsx
@@ -6,6 +6,12 @@ import styles from "./Workout.module.scss";
 // next
 import { useRouter } from "next/navigation";
 
+// context
+import { useWorkoutMode } from "@/src/context/useWorkoutMode";
+
+// components
+import WorkoutTimer from "../../../components/WorkoutTimer/WorkoutTimer";
+
 export default function Workout({
   title,
   numberOfRounds,
@@ -15,46 +21,64 @@ export default function Workout({
   roundInfo,
   data,
 }) {
+  // destructure context
+  const {
+    isWorkoutMode,
+    setIsWorkoutMode,
+    setWorkoutRounds,
+    setWorkoutRoundTime,
+    setWorkoutRestTime,
+    setWorkoutWarmupTime,
+    setSelectedWorkout,
+  } = useWorkoutMode();
+
   // init router
   const router = useRouter();
 
   console.log(data);
 
   // handle workout timer start
-  // const handleStart = () => {
-  //   setSelectedWorkout(data);
-  //   setWorkoutRounds(rounds.length);
-  //   setWorkoutRoundTime(roundTime);
-  //   setWorkoutRestTime(restTime);
-  //   setWorkoutWarmupTime(warmupTime);
-  //   setIsWorkoutMode(true);
-  //   router.push("/workouts");
-  // };
+  const handleStart = () => {
+    setSelectedWorkout(data);
+    setWorkoutRounds(numberOfRounds);
+    setWorkoutRoundTime(roundTime);
+    setWorkoutRestTime(restTime);
+    setWorkoutWarmupTime(warmupTime);
+    setIsWorkoutMode(true);
+  };
 
-  return (
-    <div className={styles.wrapper}>
-      <h1>{title}</h1>
-      <div className={styles.container}>
-        <h2>Info</h2>
-        <div>
-          <p>Total {numberOfRounds * roundTime} min</p>
-          <p>{numberOfRounds} rounds</p>
-          <p>{warmupTime} sec / warmup</p>
-          <p>{roundTime} min / round</p>
-          <p>{restTime} sec / rest</p>
-        </div>
-      </div>
-      <div className={styles.container}>
-        <h2>Combos</h2>
-        {roundInfo.map((round, index) => (
-          <div key={index}>
-            <p>
-              Round {round.round} - {round.sequence.map((seq) => `${seq} `)}
-            </p>
+  if (!isWorkoutMode) {
+    return (
+      <div className={styles.wrapper}>
+        <h1>{title}</h1>
+        <div className={styles.container}>
+          <h2>Info</h2>
+          <div>
+            <p>Total {numberOfRounds * roundTime} min</p>
+            <p>{numberOfRounds} rounds</p>
+            <p>{warmupTime} sec / warmup</p>
+            <p>{roundTime} min / round</p>
+            <p>{restTime} sec / rest</p>
           </div>
-        ))}
+        </div>
+        <div className={styles.container}>
+          <h2>Combos</h2>
+          {roundInfo.map((round, index) => (
+            <div key={index}>
+              <p>
+                Round {round.round} - {round.sequence.map((seq) => `${seq} `)}
+              </p>
+            </div>
+          ))}
+        </div>
+        <button onClick={handleStart} className={styles.startBtn}>
+          Start
+        </button>
       </div>
-      <button className={styles.startBtn}>Start</button>
-    </div>
-  );
+    );
+  }
+
+  if (isWorkoutMode) {
+    return <WorkoutTimer />;
+  }
 }
