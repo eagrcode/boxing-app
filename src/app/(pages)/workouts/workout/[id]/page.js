e7de48623a07abb79e3a6019e017245f599f851d
eagrcode <elliot.robinson92@hotmail.com>
2023-08-25 16:46:27 +0100

    add get likes and user data

--- src/app/(pages)/workouts/workout/[id]/page.js
+++ src/app/(pages)/workouts/workout/[id]/page.js
@@ -1,14 +1,35 @@
 // utils
 import getWorkoutById from "@/src/utils/getWorkoutById";
+import getWorkoutLikes from "@/src/utils/getWorkoutLikes";
 
 // components
 import Workout from "./components/Workout";
 
+// supabase client
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+
+// next
+import { cookies } from "next/headers";
+
 export default async function WorkoutPage({ params }) {
   const workoutData = await getWorkoutById(params.id);
 
+  // init supabase client
+  const supabase = createServerComponentClient({ cookies });
+
+  // get session data
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const user = session?.user;
+
+  // fetch workout likes
+  const likes = await getWorkoutLikes(params.id);
+
   return (
     <Workout
+      id={workoutData.id}
       title={workoutData.title}
       numberOfRounds={workoutData.number_of_rounds}
       roundTime={workoutData.round_time}
@@ -16,6 +37,8 @@ export default async function WorkoutPage({ params }) {
       warmupTime={workoutData.warmup_time}
       roundInfo={workoutData.round_info}
       data={workoutData}
+      likes={likes}
+      userID={user && user.id}
     />
   );
 }
