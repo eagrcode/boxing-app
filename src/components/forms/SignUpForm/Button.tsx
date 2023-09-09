b59734dfaf45bf286baa4c7784edb4b3b06c43c1
eagrcode <elliot.robinson92@hotmail.com>
2023-09-09 12:14:42 +0100

    fix prop types

--- src/components/forms/SignUpForm/Button.tsx
+++ src/components/forms/SignUpForm/Button.tsx
@@ -10,7 +10,7 @@ type ButtonProps = {
   isLoading: boolean;
 };
 
-export default function Button({ isLoading }: boolean) {
+export default function Button({ isLoading }: ButtonProps) {
   return (
     <button disabled={isLoading} type="submit" className={styles.signInBtn}>
       {isLoading ? (
