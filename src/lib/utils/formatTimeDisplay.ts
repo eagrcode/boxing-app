fbc833985159409b9be1168dbfeafd9e83b1647b
eagrcode <elliot.robinson92@hotmail.com>
2023-09-19 14:35:34 +0100

    create helper function to format time display on forms and post info

---
+++ src/lib/utils/formatTimeDisplay.ts
@@ -0,0 +1,6 @@
+// Function to format the time in "00:00" format
+export default function formatTimeDisplay(seconds: number) {
+  const mins = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
+}
