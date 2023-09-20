db3d9db6a5ac420052627ce15b28f3d768b709cf
eagrcode <elliot.robinson92@hotmail.com>
2023-09-20 11:29:57 +0100

    add text and basic styling to success page

--- src/app/(routes)/(auth)/signUp/(success-route)/success/page.tsx
+++ src/app/(routes)/(auth)/signUp/(success-route)/success/page.tsx
@@ -1,3 +1,7 @@
+// styles
+import Logo from "@/src/components/ui/Logo/Logo";
+import styles from "./page.module.scss";
+
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
@@ -17,5 +21,20 @@ export default async function SuccessPage() {
     redirect("/");
   }
 
-  return <div>SUCCESS</div>;
+  return (
+    <div className={styles.pageWrapper}>
+      <div className={styles.textContainer}>
+        <h1>Account Created!</h1>
+        <p>
+          Please click the link in the confirmation email to complete the sign-up process, then
+          close this page.
+        </p>
+        <p>
+          Please note, the sign-up flow is currently not behaving as expected, once redirected upon
+          clicking the confirmation link, you will be sent to the login page, proceed to sign in as
+          usual with the account details you've just created.
+        </p>
+      </div>
+    </div>
+  );
 }
