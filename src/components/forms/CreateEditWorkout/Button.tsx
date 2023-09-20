15bd725d61d9865d59ad3682c9d438cb49b8b186
eagrcode <elliot.robinson92@hotmail.com>
2023-09-20 10:11:30 +0100

    add loader to submit btn

--- src/components/forms/CreateEditWorkout/Button.tsx
+++ src/components/forms/CreateEditWorkout/Button.tsx
@@ -3,6 +3,9 @@
 // styles
 import styles from "./CreateEditWorkout.module.scss";
 
+// loader
+import PulseLoader from "react-spinners/PulseLoader";
+
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 
 export default function Button() {
@@ -10,7 +13,17 @@ export default function Button() {
 
   return (
     <button disabled={pending} type="submit" className={styles.submitBtn}>
-      {pending ? "Loading..." : "Post"}
+      {pending ? (
+        <PulseLoader
+          loading={pending}
+          color="var(--header-color-main)"
+          aria-label="Loading Spinner"
+          data-testid="loader"
+          size={8}
+        />
+      ) : (
+        "Post"
+      )}
     </button>
   );
 }
