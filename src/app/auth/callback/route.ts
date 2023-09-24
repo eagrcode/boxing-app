e8435b5cc03535a6b4b3cce99a1f0b1ac869b927
eagrcode <elliot.robinson92@hotmail.com>
2023-09-24 17:54:03 +0100

    clean up comments

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,28 +1,18 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = "force-dynamic";
-
-export async function GET(request: Request) {
-  // The `/auth/callback` route is required for the server-side auth flow implemented
-  // by the Auth Helpers package. It exchanges an auth code for the user's session.
-  // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
+export async function GET(req: NextRequest) {
   const supabase = createRouteHandlerClient({ cookies });
-  const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get("code");
+  const { searchParams } = new URL(req.url);
+  const code = searchParams.get("code");
 
-  console.log("REQUEST URL: ", requestUrl);
-  console.log("GET CODE: ", code);
+  console.log("CALLBACK CODE: ", code);
+  console.log(req.url);
 
-  try {
-    if (code) {
-      await supabase.auth.exchangeCodeForSession(code);
-    }
-  } catch (error: any) {
-    console.log(error.message);
+  if (code) {
+    await supabase.auth.exchangeCodeForSession(code);
   }
 
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(new URL(`${requestUrl.origin}/login`));
+  return NextResponse.redirect(new URL("/", req.url));
 }
