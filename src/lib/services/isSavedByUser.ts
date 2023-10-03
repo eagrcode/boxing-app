84bf2891255f54a92b5cce601be7888e957af0fb
eagrcode <elliot.robinson92@hotmail.com>
2023-10-03 16:43:43 +0100

    optimise performance of DB calls

--- src/lib/services/isSavedByUser.ts
+++ src/lib/services/isSavedByUser.ts
@@ -5,21 +5,21 @@ export default async function isWorkoutSaved(id: string, userID: string) {
   const supabase = createClientComponentClient<Database>();
 
   try {
-    const { data, error } = await supabase
+    const { count, error } = await supabase
       .from("user_saved_workouts")
-      .select()
-      .eq("workout_id", id)
-      .eq("user_id", userID);
+      .select("id", { count: "exact" })
+      .filter("workout_id", "eq", id)
+      .filter("user_id", "eq", userID);
 
     if (error) {
       console.log("database error: ", error);
-      return [];
+      return null;
     } else {
-      console.log("IS SAVED:", data);
-      return data;
+      console.log("IS SAVED:", count);
+      return count ? count > 0 : false;
     }
   } catch (error: any) {
     console.log("fetch error: ", error.message);
-    return [];
+    return false;
   }
 }
