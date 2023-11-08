deb64291f764e6a22e306ea3adc0d03906832cf3
eagrcode <elliot.robinson92@hotmail.com>
2023-11-08 17:59:33 +0000

    tidy up design

--- src/components/ui/WorkoutPost/WorkoutPost.tsx
+++ src/components/ui/WorkoutPost/WorkoutPost.tsx
@@ -1,30 +1,16 @@
-// styles
 import styles from "./WorkoutPost.module.scss";
-
-// next
 import Link from "next/link";
-import Image from "next/image";
 import { cookies } from "next/headers";
-
-// utils
 import getWorkoutLikesCount from "@/src/lib/services/getWorkoutLikes";
 import isSavedByUser from "@/src/lib/services/isSavedByUser";
 import isLikedByUser from "@/src/lib/services/isLikedByUser";
 import formatTimeAgo from "@/src/lib/utils/formatTimeAgo";
 import formatTimeDisplay from "@/src/lib/utils/formatTimeDisplay";
 import getWorkoutSavesCount from "@/src/lib/services/getWorkoutSaves";
-
-// components
 import SocialDataDisplay from "@/src/components/ui/SocialDataDisplay/SocialDataDisplay";
-
-// icons
-import { GiHighPunch } from "react-icons/gi";
 import { MdOutlineTimer } from "react-icons/md";
 import { BsLightningCharge, BsHourglassTop } from "react-icons/bs";
-
-// types
 import type { WorkoutPostPropTypes } from "./workoutPost.types";
-
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 export default async function WorkoutPost({
@@ -45,11 +31,6 @@ export default async function WorkoutPost({
   // init supabase client
   const supabase = createServerComponentClient({ cookies });
 
-  // get session data
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   // calc total workout time
   const totalTime = Math.floor(
     workoutWarmupTime + workoutRoundTime * workoutRounds + workoutRestTime * (workoutRounds - 1)
@@ -66,7 +47,6 @@ export default async function WorkoutPost({
       <Link className={styles.linkWrapper} href={`${variant}${id}`}>
         <div className={styles.cardTop}>
           <div className={styles.usernameContainer}>
-            {/* <GiHighPunch size={20} /> */}
             <div className={styles.avatar}>
               <div>{name?.charAt(0)}</div>
             </div>
@@ -74,25 +54,25 @@ export default async function WorkoutPost({
           </div>
           <span>{formatTimeAgo(createdAt)}</span>
         </div>
-
-        <h2 className={styles.title}>{title}</h2>
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>{title}</h2>
+        </div>
 
         <div className={styles.overview}>
           <p>{description}</p>
         </div>
+
         <div className={styles.info}>
           <div className={styles.infoDisplay}>
-            <MdOutlineTimer size={18} />
+            <MdOutlineTimer size={20} />
             <span>{formatTimeDisplay(totalTime)}</span>
           </div>
           <div className={styles.infoDisplay}>
-            <BsLightningCharge size={18} />
-            <span>
-              {workoutRounds} round{workoutRounds > 1 && "s"}
-            </span>
+            <BsLightningCharge size={20} />
+            <span>{workoutRounds}</span>
           </div>
           <div className={styles.infoDisplay}>
-            <BsHourglassTop size={16} />
+            <BsHourglassTop size={18} />
             <span>{formatTimeDisplay(workoutRoundTime)}</span>
           </div>
         </div>
