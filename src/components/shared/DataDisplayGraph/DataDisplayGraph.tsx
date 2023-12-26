b838c5b3b2e88f70527777c077802b9393867f35
eagrcode <elliot.robinson92@hotmail.com>
2023-12-26 21:15:10 +0000

    refactor to map over data array + basic styling

--- src/components/shared/DataDisplayGraph/DataDisplayGraph.tsx
+++ src/components/shared/DataDisplayGraph/DataDisplayGraph.tsx
@@ -1,13 +1,72 @@
 "use client";
 
+import { Line } from "react-chartjs-2";
+import formatGraphLabels from "@/src/lib/utils/formatGraphLabels";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 import styles from "./DataDisplayGraph.module.scss";
 
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
+type SixMonthsData = {
+  month: string;
+  entry_count: number;
+};
 
-export default function DataDisplayGraph({
-  completedWorkouts,
-}: {
-  completedWorkouts: Promise<any[]>;
-}) {
-  return <div className={styles.dataDisplayGraph}></div>;
+type GraphTypes = {
+  sixMonthsCompletedWorkouts: SixMonthsData[];
+};
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
+export const options = {
+  scales: {
+    y: {
+      ticks: {
+        color: "white",
+        stepSize: 1,
+      },
+    },
+    x: {
+      ticks: {
+        color: "white",
+      },
+    },
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Completed workouts",
+    },
+  },
+};
+
+export default function DataDisplayGraph({ sixMonthsCompletedWorkouts }: GraphTypes) {
+  const data = {
+    labels: sixMonthsCompletedWorkouts.map((entry) => formatGraphLabels(entry.month)),
+    datasets: [
+      {
+        label: "Previous 6 months",
+        data: sixMonthsCompletedWorkouts.map((entry) => entry.entry_count),
+        backgroundColor: "#0b84da",
+        borderColor: "#0b84da99",
+      },
+    ],
+  };
+
+  return (
+    <div className={styles.graphContainer}>
+      <Line options={options} data={data} />
+    </div>
+  );
 }
