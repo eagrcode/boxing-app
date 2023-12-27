1ec5d5b7ce2c83ca8832e7acb78ac98d44c8d460
eagrcode <elliot.robinson92@hotmail.com>
2023-12-27 18:57:57 +0000

    remove unused code

--- src/components/shared/Topbar/Topbar.tsx
+++ src/components/shared/Topbar/Topbar.tsx
@@ -5,31 +5,10 @@ import Logo from "../Logo/Logo";
 import Avatar from "../Avatar/Avatar";
 import { useTimerDataContext } from "@/src/context/TimerData.context";
 import { useWorkoutTimerDataContext } from "@/src/context/WorkoutTimerData.context";
-import { useAppDispatch } from "@/src/redux/hooks";
-import { useEffect } from "react";
-import { setUserDetails } from "@/src/redux/userSlice";
 
-type UserProps = {
-  userID: string;
-  fullName: string;
-  email: string;
-  avatarURL: string;
-};
-
-export default function Topbar({ userID, fullName, email, avatarURL }: UserProps) {
-  console.log("TOPBAR: ", userID, fullName, email, avatarURL);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    if (userID && fullName && email && avatarURL) {
-      dispatch(setUserDetails({ userID, fullName, email, avatarURL }));
-    }
-  }, [userID, fullName, email, avatarURL]);
-
+export default function Topbar() {
   const { isTimerActive } = useTimerDataContext();
   const { isWorkoutMode } = useWorkoutTimerDataContext();
-
-  // console.log(user);
 
   if (isTimerActive) {
     return null;
