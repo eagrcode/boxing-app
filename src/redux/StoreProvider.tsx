1d32c0103b81808fdcbdb4819ebf2ceadea94f64
eagrcode <elliot.robinson92@hotmail.com>
2023-12-27 18:50:07 +0000

    initialise store with user info from server

--- src/redux/StoreProvider.tsx
+++ src/redux/StoreProvider.tsx
@@ -3,12 +3,22 @@
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "./store";
+import { setUserDetails } from "./userSlice";
 
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
+type UserProps = {
+  userID: string;
+  fullName: string;
+  email: string;
+  avatarURL: string;
+  children: React.ReactNode;
+};
+
+export default function StoreProvider({ userID, fullName, email, avatarURL, children }: UserProps) {
   const storeRef = useRef<AppStore>();
   if (!storeRef.current) {
     // Create the store instance the first time this renders
     storeRef.current = makeStore();
+    storeRef.current.dispatch(setUserDetails({ userID, fullName, email, avatarURL }));
   }
 
   return <Provider store={storeRef.current}>{children}</Provider>;
