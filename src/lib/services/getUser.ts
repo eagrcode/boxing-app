a9fe39e2bdcf5ce9e35460531388834d1003466e
eagrcode <elliot.robinson92@hotmail.com>
2024-01-09 21:41:17 +0000

    create api call to get user

---
+++ src/lib/services/getUser.ts
@@ -0,0 +1,27 @@
+"use server";
+
+import { apiRoutes } from "../dbAPI/apiRoutes";
+import { setApiHeaders } from "../utils/setApiHeaders";
+
+export const getUser = async () => {
+  try {
+    const headers = setApiHeaders();
+
+    const res = await fetch(process.env.NEXT_PUBLIC_SUPABASE_URL + apiRoutes.getUser, {
+      headers: headers,
+      cache: "force-cache",
+    });
+
+    if (!res.ok) {
+      console.error("Error fetching user: ", res);
+    }
+
+    const data = await res.json();
+
+    console.log("GET USER: ", data);
+
+    return data;
+  } catch (error: any) {
+    console.error("Unexpected error: ", error.message);
+  }
+};
