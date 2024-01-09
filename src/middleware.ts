c494c80ae780ad06ccfc99660c8d1e1d8b54993a
eagrcode <elliot.robinson92@hotmail.com>
2024-01-09 21:34:17 +0000

    refactor logic to correctly access the presence of the user object

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,49 @@
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  console.log("MIDDLEWARE IS WORKING");
+  console.log("MIDDLEWARE IS WORKING: ", req.nextUrl.pathname);
 
-  // Paths that require authentication
-  const protectedPaths = ["/timer", "/post", "/account", "/workout"];
+  const publicRoutes: string[] = ["/", "/login", "/signUp"];
+  const isPublicRoute: boolean = publicRoutes.some((path) => req.nextUrl.pathname === path);
 
-  // Check if the current path requires authentication
-  const requiresAuthentication = protectedPaths.some((path) =>
+  const protectedRoutes: string[] = ["/dashboard", "/timer", "/create", "/profile", "/workout"];
+  const isProtectedRoute: boolean = protectedRoutes.some((path) =>
     req.nextUrl.pathname.startsWith(path)
   );
 
   const supabase = createMiddlewareClient({ req, res: NextResponse.next() });
-  const session = await supabase.auth.getSession();
 
-  if (requiresAuthentication) {
-    // Redirect to login if not authenticated
-    if (!session.data.session) {
-      return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-    }
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  console.log("Unrequired Authentication: ", isProtectedRoute);
+  console.log("Requires Authentication: ", isProtectedRoute);
+  console.log("Session: ", session);
+
+  if (isProtectedRoute && !session?.user) {
+    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  } else if (isPublicRoute && session?.user) {
+    return NextResponse.redirect(`${req.nextUrl.origin}/dashboard`);
   }
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    "/",
+
+    "/login",
+    "/signUp",
+    "/dashboard",
+    "/timer",
+    "/create",
+    "/profile",
+    "/workout",
+    // {
+    //   source: "/((?!_next/static|_next/image|favicon.ico).*)",
+    // },
+  ],
+};
