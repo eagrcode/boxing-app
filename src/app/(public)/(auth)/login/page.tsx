859c4547c1a1320273b84c4442c61cb0309aea05
eagrcode <elliot.robinson92@hotmail.com>
2024-01-11 19:08:46 +0000

    remove unused code

--- src/app/(public)/(auth)/login/page.tsx
+++ src/app/(public)/(auth)/login/page.tsx
@@ -1,15 +1,7 @@
 import styles from "./page.module.scss";
 import SignInForm from "@/src/components/forms/SignInForm/SignInForm";
-import { getSupaUser } from "@/src/lib/utils/getSupaUser";
-import { redirect } from "next/navigation";
 
-export default async function LoginPage() {
-  const user = await getSupaUser();
-
-  if (user) {
-    redirect("/");
-  }
-
+export default function LoginPage() {
   return (
     <div className={styles.wrapper}>
       <SignInForm />
--- src/app/(public)/(auth)/signUp/page.tsx
+++ src/app/(public)/(auth)/signUp/page.tsx
@@ -1,23 +1,7 @@
-// export const dynamic = "force-dynamic";
-
 import styles from "./page.module.scss";
 import SignUpForm from "@/src/components/forms/SignUpForm/SignUpForm";
-import { redirect } from "next/navigation";
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
-export default async function SignUpPage() {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
-
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  if (session?.user) {
-    redirect("/");
-  }
-
+export default function SignUpPage() {
   return (
     <div className={styles.wrapper}>
       <SignUpForm />
