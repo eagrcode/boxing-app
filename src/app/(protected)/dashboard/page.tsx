dfcc04ae622ab47bababae9a32eb8999395692f9
eagrcode <elliot.robinson92@hotmail.com>
2024-01-11 21:30:34 +0000

    implement new database function calls to allow next to cache the data

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,15 +1,17 @@
 import styles from "./page.module.scss";
 import DataDisplaySmall from "@/src/components/shared/DataDisplaySmall/DataDisplaySmall";
 import DataDisplayGraph from "@/src/components/shared/DataDisplayGraph/DataDisplayGraph";
-import getUserCompletedWorkouts from "@/src/lib/services/getUserCompletedWorkouts";
-import getUserCompletedMins from "@/src/lib/services/getUserCompletedMins";
-import getUserCompletedRounds from "@/src/lib/services/getUserCompletedRounds";
 import { GiPunchingBag } from "react-icons/gi";
 import { GiPunch } from "react-icons/gi";
 import { MdTimer } from "react-icons/md";
 import { BsFillLightningChargeFill } from "react-icons/bs";
-import { getSupaUser } from "@/src/lib/utils/getSupaUser";
-import get6MonthsData from "@/src/lib/services/get6monthsData";
+import { getTotalCompletedWorkouts } from "@/src/lib/services/dashboard/getTotalCompletedWorkouts";
+import { apiRoutes } from "@/src/lib/dbAPI/apiRoutes";
+import { getGraphData } from "@/src/lib/services/dashboard/getGraphData";
+import { getUser } from "@/src/lib/services/user/getUser";
+import { getTotalCompletedTime } from "@/src/lib/services/dashboard/getTotalCompletedTime";
+import { getTotalCompletedRounds } from "@/src/lib/services/dashboard/getTotalCompletedRounds";
+import { getAverageWorkoutLength } from "@/src/lib/services/dashboard/getAverageWorkoutLength";
 
 export default async function Index({
   searchParams,
@@ -18,62 +20,61 @@ export default async function Index({
     query?: string;
   };
 }) {
-  const user = await getSupaUser();
-
-  let userID = "";
-
-  if (user) {
-    userID = user.id;
-  }
-
-  const [completedWorkouts, completedMins, completedRounds, completedSomethingElse] =
-    await Promise.all([
-      getUserCompletedWorkouts(userID),
-      getUserCompletedMins(userID),
-      getUserCompletedRounds(userID),
-      0,
-    ]);
-
-  const completedSeconds = Math.ceil(completedMins / 60);
-
+  const user = await getUser();
   const query = searchParams?.query || "";
+
+  const [
+    cachedTotalCompletedWorkouts,
+    cachedTotalCompletedTime,
+    cachedTotalCompletedRounds,
+    cachedAverageWorkoutLength,
+  ] = await Promise.all([
+    getTotalCompletedWorkouts(apiRoutes.getTotalCompletedWorkouts(user.id)),
+    getTotalCompletedTime(apiRoutes.getTotalCompletedTime(user.id)),
+    getTotalCompletedRounds(apiRoutes.getTotalCompletedRounds(user.id)),
+    getAverageWorkoutLength(apiRoutes.getTotalCompletedTime(user.id)),
+  ]);
+
+  const sixMonthsData = await getGraphData(apiRoutes.getGraphData(query), user.id);
 
   const tiles = [
     {
       id: 1,
-      data: completedWorkouts,
+      data: cachedTotalCompletedWorkouts,
       title: "Workouts",
       icon: <GiPunch size={25} />,
+      text: "completed",
       queryParam: "get_completed_workouts",
       isActive: query === "" || query === "get_completed_workouts",
     },
     {
       id: 2,
-      data: completedSeconds,
-      title: "Minutes",
+      data: cachedTotalCompletedTime,
+      title: "Bag Time",
       icon: <MdTimer size={25} />,
+      text: "mins",
       queryParam: "get_completed_time",
       isActive: query === "get_completed_time",
     },
     {
       id: 3,
-      data: completedRounds,
+      data: cachedTotalCompletedRounds,
       title: "Rounds",
       icon: <BsFillLightningChargeFill size={25} />,
+      text: "completed",
       queryParam: "get_completed_rounds",
       isActive: query === "get_completed_rounds",
     },
     {
       id: 4,
-      data: completedSomethingElse,
-      title: "Contributions",
+      data: cachedAverageWorkoutLength,
+      title: "Avg Workout",
       icon: <GiPunchingBag size={25} />,
-      queryParam: "get_monthly_data_4",
-      isActive: query === "get_monthly_data_4",
+      text: "mins",
+      queryParam: "get_average_workout",
+      isActive: query === "get_average_workout",
     },
   ];
-
-  const sixMonthsCompletedWorkouts = await get6MonthsData(userID, query);
 
   return (
     <div className={styles.pageWrapperUser}>
@@ -85,6 +86,7 @@ export default async function Index({
             data={item.data}
             title={item.title}
             icon={item.icon}
+            text={item.text}
             queryParam={item.queryParam}
             isActive={item.isActive}
             currentQuery={query}
@@ -92,10 +94,7 @@ export default async function Index({
         ))}
       </div>
       <div className={styles.graphWrapper}>
-        <DataDisplayGraph
-          sixMonthsCompletedWorkouts={sixMonthsCompletedWorkouts}
-          currentQuery={query}
-        />
+        <DataDisplayGraph sixMonthsData={sixMonthsData} currentQuery={query} />
       </div>
     </div>
   );
