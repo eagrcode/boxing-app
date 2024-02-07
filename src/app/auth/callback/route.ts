bcc95811cb7383f19cd259ed61bbc44eeebcc69f
eagrcode <elliot.robinson92@hotmail.com>
2024-02-07 15:28:00 +0000

    remove logs

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,4 +1,3 @@
-import { Database } from "@/src/lib/database.types";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
@@ -6,9 +5,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const code = searchParams.get("code");
-
-  console.log("CALLBACK CODE: ", code);
-  console.log(req.url);
 
   if (code) {
     const cookieStore = cookies();
