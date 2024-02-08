d6638ce6b0d2ab3d94415cae83d1af68c0c71d50
eagrcode <elliot.robinson92@hotmail.com>
2024-02-08 21:26:40 +0000

    hide component when timer params are equal to active

--- src/components/shared/Topbar/Topbar.tsx
+++ src/components/shared/Topbar/Topbar.tsx
@@ -5,14 +5,16 @@ import Logo from "../Logo/Logo";
 import Avatar from "../Avatar/Avatar";
 import { useTimerDataContext } from "@/src/context/TimerData.context";
 import { useWorkoutTimerDataContext } from "@/src/context/WorkoutTimerData.context";
+import { useSearchParams } from "next/navigation";
 
 export default function Topbar() {
   const { isTimerActive } = useTimerDataContext();
   const { isWorkoutMode } = useWorkoutTimerDataContext();
 
-  if (isTimerActive) {
-    return null;
-  } else if (isWorkoutMode) {
+  const searchParams = useSearchParams();
+  const timerMode = searchParams.get("timer_mode");
+
+  if (timerMode === "active") {
     return null;
   }
 
