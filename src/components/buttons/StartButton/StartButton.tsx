64953b7c64e824d8bbb912f83308201456c19ac4
eagrcode <elliot.robinson92@hotmail.com>
2024-02-08 21:12:00 +0000

    update styling with brnd colors

--- src/components/buttons/StartButton/StartButton.tsx
+++ src/components/buttons/StartButton/StartButton.tsx
@@ -1,25 +1,10 @@
-"use client";
-
 import styles from "./StartButton.module.scss";
-import PulseLoader from "react-spinners/PulseLoader";
-import { useFormStatus } from "react-dom";
+import Button from "./Button";
 
-export default function StartButton() {
-  const { pending } = useFormStatus();
-
+export default function StartButton({ handleStart }: { handleStart: () => Promise<void> }) {
   return (
-    <button disabled={pending} type="submit" className={styles.btnStart}>
-      {pending ? (
-        <PulseLoader
-          loading={pending}
-          color="var(--header-color-main)"
-          aria-label="Loading Spinner"
-          data-testid="loader"
-          size={8}
-        />
-      ) : (
-        "START"
-      )}
-    </button>
+    <form className={styles.form} action={handleStart}>
+      <Button />
+    </form>
   );
 }
