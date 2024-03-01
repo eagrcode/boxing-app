fb822dffc6131a8ed87fbb0720ef550c23d13378
eagrcode <elliot.robinson92@hotmail.com>
2024-03-01 14:38:44 +0000

    remove unused code and import newly created components

--- src/app/(protected)/profile/(routes)/layout.tsx
+++ src/app/(protected)/profile/(routes)/layout.tsx
@@ -1,38 +1,13 @@
-"use client";
-
-import styles from "./page.module.scss";
-import UserAccountNav from "@/src/components/profile/UserAccountNav/UserAccountNav";
-import Image from "next/image";
-import { useAppSelector } from "@/src/redux/hooks";
+import ProfileBanner from "@/src/components/profile/ProfileBanner/ProfileBanner";
+import styles from "./layout.module.scss";
 
 export default function AccountLayout({ children }: { children: React.ReactNode }) {
-  const user = useAppSelector((state) => state.auth);
-
-  const { userID, fullName, email, avatarURL } = user;
-
   return (
-    <div className={styles.wrapper}>
-      <header>Account</header>
-      <div className={styles.layoutTop}>
-        <div className={styles.topLeft}>
-          <div>
-            <h1>{fullName}</h1>
-          </div>
-          <p>{email}</p>
-        </div>
-
-        {avatarURL ? (
-          <div className={styles.googleAvatar}>
-            <Image src={`${avatarURL}`} alt="User avatar" height={65} width={65} />
-          </div>
-        ) : (
-          <div className={styles.avatar}>
-            <div>{fullName.charAt(0)}</div>
-          </div>
-        )}
+    <>
+      <div className={styles.wrapper}>
+        <ProfileBanner />
       </div>
-      <UserAccountNav />
       {children}
-    </div>
+    </>
   );
 }
