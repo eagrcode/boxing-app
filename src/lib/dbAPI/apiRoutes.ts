bf3013645b8622407bbda57485ef260fbf1e241e
eagrcode <elliot.robinson92@hotmail.com>
2024-03-01 14:37:00 +0000

    create and add api query to get all user workouts

--- src/lib/dbAPI/apiRoutes.ts
+++ src/lib/dbAPI/apiRoutes.ts
@@ -9,4 +9,5 @@ export const apiRoutes = {
     `/rest/v1/rpc/${query ? query : "get_completed_workouts_6_months"}`,
   getWorkouts: `/rest/v1/rpc/get_all_workouts_with_user_details`,
   getWorkoutByID: `/rest/v1/rpc/get_workout_by_id`,
+  getUserWorkouts: `/rest/v1/rpc/get_user_workouts`,
 };
