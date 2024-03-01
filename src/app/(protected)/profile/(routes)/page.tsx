178feae8cfb22606efb0c5153a8d8f58affe09a1
eagrcode <elliot.robinson92@hotmail.com>
2024-03-01 14:39:37 +0000

    convert pages to use reusable components and adjust data fetch according to page

--- src/app/(protected)/profile/(routes)/page.tsx
+++ src/app/(protected)/profile/(routes)/page.tsx
@@ -1,19 +1,59 @@
-import { getUser } from "@/src/lib/services/user/getUser";
 import styles from "./page.module.scss";
-import UserWorkoutList from "@/src/components/profile/UserWorkoutList/UserWorkoutList";
+import WorkoutsFeed from "@/src/components/shared/WorkoutsFeed/WorkoutsFeed";
+import { apiRoutes } from "@/src/lib/dbAPI/apiRoutes";
+import { getUser } from "@/src/lib/services/user/getUser";
+import getWorkoutById from "@/src/lib/services/workout/getWorkoutById";
+import WorkoutTimer from "@/src/components/timers/WorkoutTimer/WorkoutTimer";
+import getUserWorkouts from "@/src/lib/services/profile/getUserWorkouts";
+import Workout from "@/src/components/shared/Workout/Workout";
 
-export default async function AccountPage() {
-  const user = await getUser();
+const DEFAULT_SELECTED_INDEX: number = 0;
 
-  let userID = "";
+export default async function ProfilePage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    timer_mode?: string;
+  };
+}) {
+  const { id: userID } = await getUser();
+  const query = searchParams?.query || "";
+  const timerMode = searchParams?.timer_mode || "";
 
-  if (user) {
-    userID = user.id;
-  }
+  // left view data
+  const workouts = await getUserWorkouts(apiRoutes.getUserWorkouts, userID);
+
+  const findIndex: number = workouts.findIndex(
+    (entry: { workout_id: string }) => entry.workout_id === query
+  );
+
+  // apply default index because findIndex() returns -1 if no match is found
+  const selectedIndex = findIndex === -1 ? DEFAULT_SELECTED_INDEX : findIndex;
+
+  // right view data
+  const workoutById = await getWorkoutById(
+    userID,
+    apiRoutes.getWorkoutByID,
+    query ? query : workouts[DEFAULT_SELECTED_INDEX]?.workout_id
+  );
 
   return (
-    <>
-      <UserWorkoutList userID={userID} />
-    </>
+    <div className={styles.pageWrapper}>
+      {timerMode === "active" ? (
+        <div className={styles.timerWrapper}>
+          <WorkoutTimer selectedWorkoutID={workoutById.workout_id} />
+        </div>
+      ) : (
+        <div className={styles.discoverWrapper}>
+          <div className={styles.leftView}>
+            <WorkoutsFeed workouts={workouts} selectedIndex={selectedIndex} />
+          </div>
+          <div className={styles.rightView}>
+            <Workout selectedWorkout={workoutById} />
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
--- src/app/(protected)/profile/(routes)/savedWorkouts/page.tsx
+++ src/app/(protected)/profile/(routes)/savedWorkouts/page.tsx
@@ -1,22 +1,61 @@
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-import UserSavedWorkouts from "@/src/components/profile/UserSavedWorkouts/UserSavedWorkouts";
+import styles from "../page.module.scss";
+import WorkoutsFeed from "@/src/components/shared/WorkoutsFeed/WorkoutsFeed";
+import { apiRoutes } from "@/src/lib/dbAPI/apiRoutes";
+import { getUser } from "@/src/lib/services/user/getUser";
+import getWorkoutById from "@/src/lib/services/workout/getWorkoutById";
+import WorkoutTimer from "@/src/components/timers/WorkoutTimer/WorkoutTimer";
+import Workout from "@/src/components/shared/Workout/Workout";
+import getUserSavedWorkouts from "@/src/lib/services/profile/getUserSavedWorkouts";
 
-export default async function SavedWorkoutsPage() {
-  const cookieStore = cookies();
-  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+const DEFAULT_SELECTED_INDEX: number = 0;
 
-  // get session data
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
+export default async function ProfileSavedWorkoutsPage({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    timer_mode?: string;
+  };
+}) {
+  const { id: userID } = await getUser();
+  const query = searchParams?.query || "";
+  const timerMode = searchParams?.timer_mode || "";
 
-  if (!session) {
-    redirect("/login");
-  }
+  // left view data
+  // const workouts = await getUserWorkouts(apiRoutes.getUserWorkouts, userID);
 
-  const userID: string = session.user.id;
+  const workouts = await getUserSavedWorkouts(userID);
 
-  return <UserSavedWorkouts userID={userID} />;
+  const findIndex: number = workouts.findIndex(
+    (entry: { workout_id: string }) => entry.workout_id === query
+  );
+
+  // apply default index because findIndex() returns -1 if no match is found
+  const selectedIndex = findIndex === -1 ? DEFAULT_SELECTED_INDEX : findIndex;
+
+  // right view data
+  const workoutById = await getWorkoutById(
+    userID,
+    apiRoutes.getWorkoutByID,
+    query ? query : workouts[DEFAULT_SELECTED_INDEX]?.workout_id
+  );
+
+  return (
+    <div className={styles.pageWrapper}>
+      {timerMode === "active" ? (
+        <div className={styles.timerWrapper}>
+          <WorkoutTimer selectedWorkoutID={workoutById.workout_id} />
+        </div>
+      ) : (
+        <div className={styles.discoverWrapper}>
+          <div className={styles.leftView}>
+            <WorkoutsFeed workouts={workouts} selectedIndex={selectedIndex} />
+          </div>
+          <div className={styles.rightView}>
+            <Workout selectedWorkout={workoutById} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
